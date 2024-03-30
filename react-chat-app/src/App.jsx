87703e09b318f0ed408424f3201d4b50f74c7202
saqibahmed13 <saqiba0484@gmail.com>
2024-03-30 16:40:21 +0530

    added

--- react-chat-app/src/App.jsx
+++ react-chat-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <ChatEngine
       height = "100vh"
-      projectID = "fd53ccfc-3950-4094-8520-21d7839f7871"
+      projectID = "b1d8f485-70c7-45ef-ba84-41fedd4a3cb5"
       userName = "Saqib"
       userSecret="Saqib@415160"
       renderChatFeed = {(chatAppProps)=> <ChatFeed{...chatAppProps}/>}
--- react-chat-app/src/components/ChatFeed.jsx
+++ react-chat-app/src/components/ChatFeed.jsx
@@ -4,8 +4,45 @@ import TheirMessage from "./TheirMessage"
 
 
 export default function ChatFeed(props){
+    const {chats, activeChats, userName, messages} = props;
+    const chat = chats && chats[activeChats];
+
+    const renderMessage = ()=>{
+        const keys = Object.keys(messages)
+        return keys.map((key, index)=>{
+             const message = messages[key];
+             const lastMessageKey = index ===0 ? null :keys[index-1];
+             const isMyMessage = userName === message.sender.username;
+
+             return(
+                <div key={`msg_${index}`} style={{width:"100%"}}>
+                <div className="message-block">
+                    {
+                        isMyMessage
+                        ?<MyMessage/>:<TheirMessage/>
+                    }
+                </div>
+                <div className="read-receipts" style={{marginRight:isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                 read-receipts
+                </div>
+
+                </div>
+             )
+        });
+    }
+
+    renderMessage();
+
     return (
-        <div>
+        <div className="chat-feed">
+        <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+        {chat.people.map((person)=> `${person.person.username}`)}
+
+        </div>
+
+        </div>
             ChatFeed
         </div>
     )
